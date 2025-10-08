d17a775a3beedebc787e6ad6b6812ad08e4aa4c9
S <samuelfilipe087@gmail.com>
2025-10-08 18:08:35 -0300

    Add files via upload
    Este projeto, foi onde utilizei pela primeira vez o conceito de métodos, um exemplo foi para escolher bebidas. Em breve irei aprendendo mais e trazendo algo mais elaborado...

---
+++ BebidasComMetodo.java
@@ -0,0 +1,26 @@
+package projetov2;
+
+import java.util.Scanner;
+
+public class BebidasComMetodo {
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		String[] listaBebidas = {"Àgua", "Refrigerante", "Suco de uva", "Café"};
+		
+		for (int variavelFor = 0; variavelFor < listaBebidas.length; variavelFor++) { 
+			System.out.println("(" +variavelFor+ ") "+ "Bebida:" +listaBebidas[variavelFor]);
+		} impressaoBarras();
+			impressaoDigitarNumero();
+		Integer receberNumero = scanner.nextInt();
+		
+		System.out.print("Você escolheu a bebida " +listaBebidas[receberNumero]);
+	}
+		String[] listaBebidas = {"Àgua, Refrigerante, Café, Suco de uva"};
+ 	
+	static void impressaoBarras() {
+		System.out.println("//////////////////////////////////////////////////////////");
+	}
+	static void impressaoDigitarNumero() {
+		System.out.print("Digite o número de qual bebida deseja: ");
+	}
+}
