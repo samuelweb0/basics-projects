30542d71111f65f848b3ebb2e68bba499be347ab
S <samuelfilipe087@gmail.com>
2026-01-12 23:32:58 -0300

    Add files via upload
    Neste código, possui a utilização de métodos de instância.

--- ValidationStock.java
+++ ValidationStock.java
@@ -1,29 +1,16 @@
 package ProductsStore;
-
-import java.util.Scanner;
 public class ValidationStock  {
 	public static void main(String[] args) {
 	
 	ProductsValue productsValue = new ProductsValue();
-	Scanner scanner = new Scanner(System.in);
 	productsValue.name = "Ferro";
 	productsValue.minimumQuantity = 10;
-	productsValue.quantityStock = 0;
+	productsValue.quantityStock = 90;
 	
 	System.out.println("Produto: " +productsValue.name);
 	System.out.println("Quantidade mínima: " +productsValue.minimumQuantity);
 	System.out.println("Quantidade atual: " +productsValue.quantityStock);
 
-	System.out.println("Reposição desejada: " +bringingReplacement(productsValue));
-	
-	scanner.close();
-	}
-	static boolean bringingReplacement (ProductsValue productsValue) {
-	if (productsValue.quantityStock < productsValue.minimumQuantity) {
-	return true;	
-	}
-	return false;
+	System.out.println("Reposição desejada: " +productsValue.bringingReplacement());
 	}	
 	}
-	
-
