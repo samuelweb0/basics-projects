2248038f7ab4ca564dbff6b68f78d38b57953a6f
S <samuelfilipe087@gmail.com>
2026-01-12 23:38:19 -0300

    Update and rename UtilizandoFor.java to UsingFor.java
    Atualizando para o inglês.

--- UsingFor.java
+++ UsingFor.java
@@ -6,21 +6,20 @@ public class UtilizandoFor {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
 		
-		String[] nomesFuncionarios = {"Heloisa, Yasmin, Felipe, Ronaldo, Yuri"};
+		String[] namesEmployees = {"Heloisa, Yasmin, Felipe, Ronaldo, Yuri"};
 		for(int variavelFor = 0; variavelFor < nomesFuncionarios.length; variavelFor++) { 
 			
 			System.out.print("Digite a pontuação atingida pelo grupo de "+nomesFuncionarios[variavelFor]+ ": " );
-			Integer valorMeta = scanner.nextInt();
-			Integer metaMinima = 550;
+			Integer targetValue = scanner.nextInt();
+			Integer targetMinimal = 550;
 			
-			Boolean metaMaior = valorMeta >= metaMinima;
-			if (metaMaior) {
+			Boolean targetLargest = targetValue >= targetMinimal;
+			if (targetLargest) {
 				System.out.println("O grupo de " +nomesFuncionarios[variavelFor]+ " teve sua meta atingida.");
 			} else {
 				System.out.println("O grupo de " +nomesFuncionarios[variavelFor]+ " não teve sua meta atingida."); 
 			}
-		}
-		
+		}	
 		scanner.close();
 	}
 }
