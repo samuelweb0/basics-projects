6d701150afb2c0b5fe2c0d09777579c64082caaf
S <samuelfilipe087@gmail.com>
2026-04-27 22:39:48 -0300

    Update and rename NotaAluno.java to gradeStudents.java
    Correção de identação, mudança de nomes de váriaveis e classe.

--- gradeStudents.java
+++ gradeStudents.java
@@ -2,23 +2,23 @@ package projetov2;
 
 import java.util.Scanner;
 
-public class NotaAluno {
+public class gradeStudents {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		Boolean VerdadeiraNota= true;
-		Boolean FalsaNota= false;
-					
+		
+		Boolean trueGrade= true;
+		Boolean falseGrade= false;
 		System.out.println("Qual é o nome do aluno? ");
-		String Aluno = scannear.nextLine();
+		String student = scannear.nextLine();
+		
 		System.out.println("Digite a nota obtida por este aluno no bimestre:");
-		Integer Numero = scannear.nextInt();	
-		Boolean Nota = Numero >=70;
-		if (Nota) {
+		Integer number = scannear.nextInt();	
+		Boolean grade = number >= 70;
+		if (grade) {
 		System.out.println("Parabéns, o aluno(a) "+ Aluno +", passou de ano!");
 		} else {
 		System.out.println("Que pena! O aluno(a) " + Aluno +", não passou de ano." );
-		}
-				
+		}		
 		scanner.close();		
-		}
+			}	
 		}
