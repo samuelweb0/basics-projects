4e0b97097ba0053482dc1161a62e95ada221ac72
S <samuelfilipe087@gmail.com>
2026-04-27 22:31:45 -0300

    Update Desconto100Reais.java
    Mudança de nomes de classes.

--- Desconto100Reais.java
+++ Desconto100Reais.java
@@ -2,7 +2,7 @@ package projetov2;
 
 import java.util.Scanner;
 
-public class Desconto100Reais {
+public class FreightOrNo {
 		public static void main(String[] args) {
 			Scanner scanner = new Scanner(System.in);
 			
